5e2474ef4735f8af209ab2307a807b91aa8ca7d5
zshdevnet <zshdev.net@gmail.com>
2025-05-04 17:10:04 -0400

    OOP 10%

---
+++ oop.py
@@ -0,0 +1,26 @@
+class BankAccount:
+    def __init__(self):
+        self.__balance = 0  # private variable
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def cashup(self,amount):
+        self.__balance -= amount
+
+    def get_balance(self):
+        return self.__balance
+
+acc = BankAccount()
+
+try:
+    asking = input("For Deposit input 'D' for cashup input 'C': ")
+    
+    if asking == ('C' and 'c'):
+        acc.cashup(int(input("The amount you want to cashup: ")))
+        print(acc.get_balance())
+    elif asking == 'D' or asking == 'd':
+        acc.deposit(int(input("The amount you want to deposit: ")))
+        print(acc.get_balance())
+except:
+    print("something goes wrong, please try in a moment !")
